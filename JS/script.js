2f42f46b153211ce2831fd083daa58935abc0668
terrarian1234 <164236579+terrarian1234@users.noreply.github.com>
2024-06-04 22:16:15 +0400

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+let myobject = {
+    firstName : 'luka',
+    lastName :  'uriadmkopeli',
+    age : '14',
+    phoneNumbers :[
+        {pirveli:567131502},
+        {meore: 567131503}
+    ]
+}
+console.log(myobject)
